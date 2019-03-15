e84bc66c4ab366c45f5657a7672c850ddec43e5d
longjin <1176837406@qq.com>
2019-03-15 22:05:49 +0800

    '8.5'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,12 +2,13 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hotCities="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     import CityHeader from "./component/Header";
     import CitySearch from "./component/Search";
     import CityList from "./component/List";
@@ -16,12 +17,34 @@
 
     export default {
         name: "City",
+        data () {
+            return{
+                cities:{},
+                hotCities:[]
+            }
+        },
         components: {
             CityAlphabet,
             CityList,
             CityHeader,
             CitySearch
         },
+        methods:{
+            getCityInfo (){
+                axios.get('/static/mock/city.json').then(this.handleGetCityInfoSuccess)
+            },
+            handleGetCityInfoSuccess (res) {
+                res = res.data
+                if (res.ret && res.data) {
+                    const data = res.data;
+                    this.cities=data.cities;
+                    this.hotCities=data.hotCities
+                }
+            }
+        },
+        mounted() {
+            this.getCityInfo()
+        }
     }
 </script>
 
