ed32f9daddb8ba1697d3fd36890d4eb528f02651
Hello <1176837406@qq.com>
2019-03-15 10:05:22 +0800

    '7.7'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,8 @@
         <home-header></home-header>
         <home-swiper></home-swiper>
         <home-icons></home-icons>
+        <home-reommend></home-reommend>
+        <home-weekend></home-weekend>
     </div>
 </template>
 
@@ -11,12 +13,16 @@
     import HomeHeader from './components/Header'
     import HomeSwiper from './components/Swiper'
     import HomeIcons from './components/Icons'
+    import HomeReommend from './components/Reommend'
+    import HomeWeekend from './components/Weekend'
     export default {
         name: "Home",
         components: {
             HomeHeader,
             HomeSwiper,
-            HomeIcons
+            HomeIcons,
+            HomeReommend,
+            HomeWeekend
         }
     }
 </script>
--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -92,45 +92,46 @@
 
 <style lang="stylus" scoped>
     @import "~@/assets/style/varibles.styl"
-    @import "~@/assets/style/mixmins.styl"
+    @import "~@/assets/style/mixins.styl"
     .icons >>> .swiper-container
         height 0
         padding-bottom 50%
+    .icons
+        margin-top .1rem
 
+        .icon
+            position relative
+            overflow hidden
+            float left
+            width 25%
+            height 0
+            /*background red*/
+            padding-bottom 25%
 
-    .icon
-        position relative
-        overflow hidden
-        float left
-        width 25%
-        height 0
-        /*background red*/
-        padding-bottom 25%
-
-        .icon-img
-            position absolute
-            top 0
-            left 0
-            right 0
-            bottom .44rem
-            box-sizing border-box
-            padding .1rem
-
-            .icon-img-content
-                display block
-                margin 0 auto
-                height 100%
-
-            .icon-desc
+            .icon-img
                 position absolute
-                bottom 0
+                top 0
                 left 0
                 right 0
-                height .44rem
-                line-height .44rem
-                text-align center
-                color $darkTextColor
-                ellipsis()
+                bottom .44rem
+                box-sizing border-box
+                padding .1rem
+
+                .icon-img-content
+                    display block
+                    margin 0 auto
+                    height 100%
+
+                .icon-desc
+                    position absolute
+                    bottom 0
+                    left 0
+                    right 0
+                    height .44rem
+                    line-height .44rem
+                    text-align center
+                    color $darkTextColor
+                    ellipsis()
 
 
 
