4d0cf7452309be1aeeca064f88295b641d20d167
longjin <1176837406@qq.com>
2019-03-16 14:56:47 +0800

    '8.7'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list :cities="cities" :hotCities="hotCities"></city-list>
-        <city-alphabet :cities="cities"></city-alphabet>
+        <city-list :cities="cities" :hotCities="hotCities" :letter="letter"></city-list>
+        <city-alphabet :cities="cities" @change="handLeLetterChange"></city-alphabet>
     </div>
 </template>
 
@@ -17,10 +17,11 @@
 
     export default {
         name: "City",
-        data () {
-            return{
-                cities:{},
-                hotCities:[]
+        data() {
+            return {
+                cities: {},
+                hotCities: [],
+                letter: ''
             }
         },
         components: {
@@ -29,17 +30,20 @@
             CityHeader,
             CitySearch
         },
-        methods:{
-            getCityInfo (){
+        methods: {
+            getCityInfo() {
                 axios.get('/static/mock/city.json').then(this.handleGetCityInfoSuccess)
             },
-            handleGetCityInfoSuccess (res) {
+            handleGetCityInfoSuccess(res) {
                 res = res.data
                 if (res.ret && res.data) {
                     const data = res.data;
-                    this.cities=data.cities;
-                    this.hotCities=data.hotCities
+                    this.cities = data.cities;
+                    this.hotCities = data.hotCities
                 }
+            },
+            handLeLetterChange(letter) {
+                this.letter = letter
             }
         },
         mounted() {
--- src/pages/city/component/List.vue
+++ src/pages/city/component/List.vue
@@ -21,11 +21,12 @@
 
             </div>
 
-            <div class="area" v-for="(item,key) of cities" :key="key">
+            <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
 
                 <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
-                    <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
+                    <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}
+                    </div>
                 </div>
             </div>
 
@@ -40,10 +41,21 @@
         name: "CityList",
         props: {
             hotCities: Array,
-            cities: Object
+            cities: Object,
+            letter: String
         },
         mounted() {
+
             this.scroll = new Bscroll(this.$refs.wrapper)
+        },
+        //监听器
+        watch: {
+            letter() {
+                if (this.letter) {
+                    const element = this.$refs[this.letter][0];
+                    this.scroll.scrollToElement(element)
+                }
+            }
         }
     }
 </script>
