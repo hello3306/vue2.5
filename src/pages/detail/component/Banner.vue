0c542d69888ad75072427b6e99828c12e14d24ed
Hello <1176837406@qq.com>
2019-03-18 11:49:57 +0800

    '9.2'

--- src/pages/detail/component/Banner.vue
+++ src/pages/detail/component/Banner.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="banner">
+        <div class="banner" @click="handleBannerClick">
             <img class="banner-img"
                  src="http://img1.qunarzz.com/sight/p0/1501/b5/b536539824a83cb3.water.jpg_710x420_f9ffc62d.jpg"/>
             <div class="banner-info">
@@ -8,7 +8,7 @@
                 <div class="banner-number banner-icon"><span class="iconfont arrow-icon">&#xe7b3;</span> 33</div>
             </div>
         </div>
-        <common-gallary></common-gallary>
+        <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryGlose"></common-gallary>
     </div>
 </template>
 
@@ -17,7 +17,24 @@
 
     export default {
         name: "Banner",
-        components: {CommonGallary},
+        components: {
+            CommonGallary
+        },
+        data() {
+            return {
+                showGallary: false,
+                imgs: ['http://img1.qunarzz.com/sight/p0/1704/b7/b7dba076cbb4f40da3.water.jpg_r_800x800_756229ed.jpg',
+                    'http://img1.qunarzz.com/sight/p0/1704/d6/d6ab256208190418a3.water.jpg_r_800x800_96da9dc0.jpg']
+            }
+        },
+        methods: {
+            handleBannerClick() {
+                this.showGallary = true
+            },
+            handleGallaryGlose() {
+                this.showGallary = false
+            }
+        },
 
     }
 </script>
